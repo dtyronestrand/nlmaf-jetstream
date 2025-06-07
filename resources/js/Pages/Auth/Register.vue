02a85ad7d8a7bab87a6b65c734e6b99dcbbee62b
dtyronestrand <noreply@4961776.xyz>
2025-06-07 01:17:43 -0400

    regitration page styling

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -37,12 +37,12 @@ const submit = () => {
                     id="first_name"
                     v-model="form.first_name"
                     type="text"
-                    class="mt-1 block w-full"
+                    class="mt-1 text-[var(--color-neutral-900)] block w-full"
                     required
                     autofocus
                     autocomplete="first-name"
                 />
-                <InputError class="mt-2" :message="form.errors.first_name" />
+                <InputError class="mt-2  text-[var(--color-primary-300)]" :message="form.errors.first_name" />
             </div>
             <div class="mt-4">
                 <InputLabel for="last_name" value="Last Name" />
@@ -50,11 +50,11 @@ const submit = () => {
                     id="last_name"
                     v-model="form.last_name"
                     type="text"
-                    class="mt-1 block w-full"
+                    class="mt-1 block text-[var(--color-neutral-900)] w-full"
                     required
                     autocomplete="last-name"
                 />
-                <InputError class="mt-2" :message="form.errors.last_name" />
+                <InputError class="mt-2  text-[var(--color-primary-300)]" :message="form.errors.last_name" />
 </div>
             <div class="mt-4">
                 <InputLabel for="email" value="Email" />
@@ -62,11 +62,11 @@ const submit = () => {
                     id="email"
                     v-model="form.email"
                     type="email"
-                    class="mt-1 block w-full"
+                    class="mt-1 block text-[var(--color-neutral-900)] w-full"
                     required
                     autocomplete="username"
                 />
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class=" text-[var(--color-primary-300)] mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
@@ -75,11 +75,11 @@ const submit = () => {
                     id="password"
                     v-model="form.password"
                     type="password"
-                    class="mt-1 block w-full"
+                    class="mt-1 block text-[var(--color-neutral-900)] w-full"
                     required
                     autocomplete="new-password"
                 />
-                <InputError class="mt-2" :message="form.errors.password" />
+                <InputError class=" text-[var(--color-primary-300)] mt-2" :message="form.errors.password" />
             </div>
 
             <div class="mt-4">
@@ -88,11 +88,11 @@ const submit = () => {
                     id="password_confirmation"
                     v-model="form.password_confirmation"
                     type="password"
-                    class="mt-1 block w-full"
+                    class="mt-1 text-[var(--color-neutral-900)] block w-full"
                     required
                     autocomplete="new-password"
                 />
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                <InputError class=" text-[var(--color-primary-300)] mt-2" :message="form.errors.password_confirmation" />
             </div>
 
             <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
@@ -104,7 +104,7 @@ const submit = () => {
                             I agree to the <a target="_blank" :href="route('terms.show')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Terms of Service</a> and <a target="_blank" :href="route('policy.show')" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Privacy Policy</a>
                         </div>
                     </div>
-                    <InputError class="mt-2" :message="form.errors.terms" />
+                    <InputError class=" text-[var(--color-primary-300)] mt-2" :message="form.errors.terms" />
                 </InputLabel>
             </div>
 
